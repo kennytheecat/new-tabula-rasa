13a4610f99e8f8670c3e1c723689080b3f6e8b8e
City of Prescott <webmaster@prescott-az.gov>
2014-06-30 14:42:15 -0700

    connected admin less mixens to regular mixins

--- inc/functions-admin.php
+++ inc/functions-admin.php
@@ -247,7 +247,7 @@ function load_custom_wp_admin_style() {
 	wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/css/admin.css', false, '1.0.0' );
 	wp_enqueue_style( 'custom_wp_admin_css' );
 }
-//add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 
 /**  Custom Backend Footer
 **************************************************************/
