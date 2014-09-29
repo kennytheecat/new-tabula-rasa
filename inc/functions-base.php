7d52ae5d15885e292c58b1439a0758012a7a8ce9
City of Prescott <webmaster@prescott-az.gov>
2014-09-29 16:59:26 -0700

    Fixed internal links being display during print

--- inc/functions-base.php
+++ inc/functions-base.php
@@ -407,4 +407,37 @@ class Clean_404_Email {
 	}
  
 } // Clean_404_Email
+
+//Add QR Code to print
+
+function insertPrintOnlyCSS() {
+	echo '
+<!-- QR Print Styles -->
+<style type="text/css">
+.QRprintonly {display:none;}
+</style>
+	';
+	echo '
+<style type="text/css" media="print">
+.QRprintonly {display:block !important;}
+</style>
+	';
+}
+
+function insertQRCode() {
+ 	$permalink = "http" . ((!empty($_SERVER['HTTPS'])) ? "s" : "") . "://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+ 	// QR code url
+ 	$qr_code_url = 'http://chart.apis.google.com/chart?chs=200x200&cht=qr&chld=|0&chl='.urlencode($permalink);
+ 	echo '
+<!-- QR Print -->
+<div class="QRprintonly">
+	<p>Printed from: '.$permalink.'</p>
+	<p>Scan to visit this page:</p>
+	<img src="'.$qr_code_url.'" />
+</div>
+';
+}
+
+add_action('wp_head', 'insertPrintOnlyCSS');
+add_action('wp_footer', 'insertQRCode');
 ?>
